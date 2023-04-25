d5e376d1255aa02b1331c522b8bfc55d1fe3cc6b
Bram-ter <bram.tersteeg2@hva.nl>
2023-04-25 14:34:03 +0200

    add copy buttons

---
+++ js/copyContent.js
@@ -0,0 +1,63 @@
+// * Globals * //
+
+const alert = document.querySelector("body span");
+alert.style.display = "none";  
+
+// * Menu * //
+const menu = document.querySelectorAll("#copyButtons div");
+const menuItems = document.querySelectorAll("#copyButtons div ul li");
+
+menuItems.forEach(item => {
+  item.style.display = "none";
+});
+
+menu.forEach(item => {
+  item.addEventListener("click", showItems);
+});
+
+function showItems (event) {
+  // Get the clicked <ul> element
+  const clickedMenu = event.currentTarget;
+
+  // Get the <li> elements within the clicked <ul>
+  const clickedMenuItems = clickedMenu.querySelectorAll("div ul li");
+
+  // Show the <li> elements within the clicked <ul>
+  clickedMenuItems.forEach(item => {
+    item.classList.toggle("display");
+  });
+}
+
+// * Copy whole section * //
+
+const copyButton = document.querySelector("#copyButtons div:nth-of-type(1)");
+
+copyButton.addEventListener("click", copySection);
+
+function copySection() {
+  // Get the section element
+  const section = document.querySelector("section:nth-of-type(1)");
+
+  // Show the alert
+  alert.innerHTML = "Sectie gekopieerd!";
+  alert.style.display = "block";
+  
+  // Create a range object to select the section content
+  const range = document.createRange();
+  range.selectNode(section);
+
+  // Add the range to the clipboard
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+  document.execCommand("copy");
+
+  // Deselect the range
+  selection.removeAllRanges();
+
+  // Remove the alert
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 2000);
+}
+
---
+++ js/observer.js
@@ -0,0 +1,65 @@
+const sections = document.querySelectorAll('section');
+const buttons = document.querySelectorAll('#scrollButtons li');
+let currentSectionIndex = 0;
+let activeHeading = sections[currentSectionIndex].querySelector('h1'); // store the active heading element
+
+// Create an IntersectionObserver
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // Remove the active class from all sections
+      sections.forEach(section => {
+        section.classList.remove('active');
+      });
+
+      // Add the active class to the intersecting section
+      entry.target.classList.add('active');
+      // Update the currentSectionIndex
+      currentSectionIndex = Array.from(sections).indexOf(entry.target);
+
+      // Update the active heading element
+      activeHeading = entry.target.querySelector('h1');
+    }
+  });
+});
+
+// Observe each section
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+// Add click event listeners to the buttons
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    if (index === 0) {
+      // Scroll to the previous section
+      currentSectionIndex--;
+    } else if (index === 1) {
+      // Scroll to the next section
+      currentSectionIndex++;
+    }
+    // Make sure the currentSectionIndex is within the range of sections
+    currentSectionIndex = Math.max(0, Math.min(currentSectionIndex, sections.length - 1));
+    // Scroll to the current section
+    sections[currentSectionIndex].scrollIntoView({ behavior: 'smooth' });
+
+    // Update the active heading element
+    activeHeading = sections[currentSectionIndex].querySelector('h1');
+  });
+});
+
+// * Copy * //
+
+// select the list items
+const listItems = document.querySelectorAll('#copyButtons div ul li');
+
+// add click event listeners to each list item
+listItems.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    // select the corresponding heading text
+    const headingText = activeHeading.textContent;
+    
+    // copy the text to the clipboard
+    navigator.clipboard.writeText(headingText);
+  });
+});
